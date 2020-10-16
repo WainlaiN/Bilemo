c287444e47de5612c4effa607eb93354eda6ba6c
Nicolas <nicodupriez@hotmail.com>
2020-10-16 10:20:31 +0200

    Added user show & put

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -30,6 +32,16 @@ class ApiUserController extends AbstractController
     }
 
     /**
+     * @Route("api/user/{id}", name="api_user_show", methods={"GET"})
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function show(User $user)
+    {
+        return $this->json($user, 200, [], ['groups' => 'user:read']);
+    }
+
+    /**
      * @Route("/api/user", name="api_user_create", methods={"POST"})
      * @param Request $request
      * @param SerializerInterface $serializer
@@ -41,7 +53,8 @@ class ApiUserController extends AbstractController
         Request $request,
         SerializerInterface $serializer,
         EntityManagerInterface $manager,
-        ValidatorInterface $validator
+        ValidatorInterface $validator,
+        UrlGeneratorInterface $urlGenerator
     ) {
         $json = $request->getContent();
 
@@ -58,7 +71,14 @@ class ApiUserController extends AbstractController
             $manager->persist($user);
             $manager->flush();
 
-            return $this->json($user, 201, [], ['groups' => 'user:read']);
+            return $this->json(
+                $user,
+                201,
+                [
+                    "Location" => $urlGenerator->generate("api_user_show", ["id" => $user->getId()]),
+                ],
+                ['groups' => 'user:read']
+            );
 
         } catch (NotEncodableValueException $e) {
             return $this->json(
@@ -66,11 +86,47 @@ class ApiUserController extends AbstractController
                     'status' => 400,
                     'message' => $e->getMessage(),
                 ],
-                400
+                400,
+
             );
 
         }
+    }
 
+    /**
+     * @Route("api/user/{id}", name="api_user_put", methods={"PUT"})
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $manager
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
+    public function put(
+        User $user,
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $manager,
+        ValidatorInterface $validator
+    ) {
+
+        $json = $request->getContent();
+        $user = $serializer->deserialize(
+            $json,
+            User::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $user]
+        );
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
+
+        $manager->persist($user);
+        $manager->flush();
+
+        return $this->json(null, 204, []);
 
     }
 }
