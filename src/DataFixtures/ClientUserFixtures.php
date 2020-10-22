33201f4bf3c63edc356a1bbfdd4cc1e941f5d24e
Nicolas <nicodupriez@hotmail.com>
2020-10-22 18:03:48 +0200

    set Client as security providers + fixtures for password

--- src/DataFixtures/ClientUserFixtures.php
+++ src/DataFixtures/ClientUserFixtures.php
@@ -8,6 +8,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class ClientUserFixtures extends Fixture
@@ -20,8 +21,17 @@ class ClientUserFixtures extends Fixture
         'Orange',
         'SFR',
         'Virgin',
-        'Bouygues'
+        'Bouygues',
+        'Cdiscount',
+        'LDLC'
     ];
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
 
     public function load(ObjectManager $manager)
     {
@@ -29,17 +39,19 @@ class ClientUserFixtures extends Fixture
         $this->faker = Factory::create('fr_FR');
 
         //create clients
-        for ($i = 0 ; $i <= 3; $i++) {
+        for ($i = 0; $i <= 5; $i++) {
 
             $client = new Client();
             $client->setName(self::$name[$i]);
+            $client ->setEmail(self::$name[$i]."@gmail.com");
+            $client->setPassword($this->encoder->encodePassword($client, self::$name[$i]));
 
             $manager->persist($client);
             $clients[] = $client;
         }
 
         //create users
-        for ($i = 0 ; $i <= 10 ; $i++) {
+        for ($i = 0; $i <= 50; $i++) {
 
             $user = new User();
             $user->setUsername($this->faker->userName)
--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -7,13 +7,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ClientRepository::class)
  * @UniqueEntity("name", message="ce client existe dejà")
  */
-class Client
+class Client implements UserInterface
 {
     /**
      * @ORM\Id
@@ -31,10 +32,24 @@ class Client
     private $name;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("client:read")
+     * @Groups("user:read")
+     */
+    private $email;
+
+
+    /**
      * @ORM\OneToMany(targetEntity=User::class, mappedBy="client", cascade={"persist", "remove"})
      * @Groups("client:read")
      */
     private $users;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $password;
+
 
     public function __construct()
     {
@@ -82,5 +97,57 @@ class Client
         }
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
