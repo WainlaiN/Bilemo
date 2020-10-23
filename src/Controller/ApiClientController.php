0742bf3a98f2ff3a629152f1d53c302609920a1b
Nicolas <nicodupriez@hotmail.com>
2020-10-23 18:15:04 +0200

    Installed JWT, entity Client modified, Fixtures OK

--- src/Controller/ApiClientController.php
+++ src/Controller/ApiClientController.php
@@ -4,13 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Repository\ClientRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Class ApiClientController
--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -9,12 +9,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
@@ -35,7 +32,7 @@ class ApiUserController extends AbstractController
     }
 
     /**
-     * @Route("api/client/{id}", name="api_client_show", methods={"GET"})
+     * @Route("api/user/{id}", name="api_client_show", methods={"GET"})
      * @param User $user
      * @return JsonResponse
      *
