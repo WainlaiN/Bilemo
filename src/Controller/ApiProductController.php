4a5019251208540c36c5bde62ee87b2e18325654
Nicolas <nicodupriez@hotmail.com>
2020-10-26 16:23:54 +0100

    Nelmio installed for PHPDoc

--- src/Controller/ApiProductController.php
+++ src/Controller/ApiProductController.php
@@ -4,16 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 
 class ApiProductController extends AbstractController
 {
     /**
+     * List all products.
+     *
+     * This call display all products.
+     *
      * @Route("/api/product", name="api_product_index", methods={"GET"})
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the products list",
+     *     @OA\JsonContent(type="string", description="test"),
+     *     @OA\Schema(
+     *         type="array",
+     *         @OA\Items(ref=@Model(type=Product::class, groups={"product:read"}))
+     *     )
+     * )
+     *
      * @param ProductRepository $productRepository
      * @return JsonResponse
      */
@@ -25,7 +42,22 @@ class ApiProductController extends AbstractController
     }
 
     /**
+     * Product detail.
+     *
+     * This call display a product detail.
+     *
      * @Route("api/product/{id}", name="api_product_show", methods={"GET"})
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the product detail",
+     *     @OA\Schema(
+     *         type="array",
+     *         @OA\Items(ref=@Model(type=Product::class, groups={"product:read"}))
+     *     )
+     * )
+     *
+     *
      * @param Product $product
      * @return JsonResponse
      */
