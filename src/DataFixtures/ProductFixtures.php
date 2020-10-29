827c1da799c3b97ff1ab1a79469897f000f8711a
Nicolas <nicodupriez@hotmail.com>
2020-10-29 09:37:21 +0100

    New fixtures for cache test

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -13,9 +13,28 @@ class ProductFixtures extends Fixture
         'iPhone SE',
         'iPhone 11',
         'iPhone XS Max',
+        'iPhone 11 Pro',
+        'iPhone 11 Pro Max',
+        'iPhone 12',
+        'iPhone 12 Mini',
+        'iPhone 12 Pro',
+        'Galaxy S10 Lite',
+        'Galaxy S10e',
+        'Galaxy S10+',
+        'Galaxy S20 FE',
         'Galaxy S20',
         'Galaxy S20+',
-        'Galaxy Note 20'
+        'Galaxy S20 Ultra',
+        'Galaxy Note 20',
+        'Huawei P30',
+        'Huawei P30 Pro',
+        'Huawei Mate 30',
+        'Huawei Mate 30 Pro',
+        'Huawei P40',
+        'Huawei P40 Pro',
+        'Huawei mate 40',
+        'Huawei mate 40 Pro',
+
     ];
 
     private static $brand = [
@@ -23,9 +42,27 @@ class ProductFixtures extends Fixture
         'Apple',
         'Apple',
         'Apple',
+        'Apple',
+        'Apple',
+        'Apple',
+        'Apple',
+        'Apple',
         'Samsung',
         'Samsung',
         'Samsung',
+        'Samsung',
+        'Samsung',
+        'Samsung',
+        'Samsung',
+        'Samsung',
+        'Huawei',
+        'Huawei',
+        'Huawei',
+        'Huawei',
+        'Huawei',
+        'Huawei',
+        'Huawei',
+        'Huawei'
     ];
 
     private static $price = [
@@ -40,12 +77,12 @@ class ProductFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i <= 6; $i++) {
+        for ($i = 0; $i <= 24; $i++) {
 
              $product = new Product();
              $product->setBrand(self::$brand[$i])
                  ->setModel(self::$model[$i])
-                 ->setPrice(self::$price[$i]);
+                 ->setPrice(self::$price[array_rand(self::$price)]);
 
              $manager->persist($product);
 
