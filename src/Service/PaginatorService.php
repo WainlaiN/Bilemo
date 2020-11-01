833f9e3894de7891e71f515296de6e58ccd2ebc9
Nicolas <nicodupriez@hotmail.com>
2020-11-01 14:03:38 +0100

    Check number of users page available and throw http exception

--- src/Service/PaginatorService.php
+++ src/Service/PaginatorService.php
@@ -6,6 +6,7 @@ namespace App\Service;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class PaginatorService
@@ -22,25 +23,33 @@ class PaginatorService
 
         $paginator = new Paginator($query);
 
-        $totalItems = $this->total($paginator);
-
-        $pagesCount = ceil($totalItems / $pageSize);
+        //$totalItems = $this->total($paginator);
+        //$pagesCount = ceil($totalItems / $pageSize);
 
         // now get one page's items:
-
         $data = $paginator
             ->getQuery()
             ->setFirstResult($pageSize * ($page - 1)) // set the offset
             ->setMaxResults($pageSize) // set the limit
             ->getResult();
 
+        if ($page <= $this->lastPage($paginator)) {
 
-        return $data;
+            return $data;
+
+        }
+
+        throw new NotFoundHttpException($this->lastPage($paginator) ." pages disponibles");
     }
 
     public function total(Paginator $paginator)
     {
         return $paginator->count();
+    }
+
+    public function lastPage(Paginator $paginator)
+    {
+        return ceil($paginator->count() / $paginator->getQuery()->getMaxResults());
     }
 
 
